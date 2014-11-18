70ae8abbf7bd301b2cc0bd2d8f19718eb80c4977
Miasma <chjacobsen.se@gmail.com>
2014-11-18 22:44:02 +0100

    Sample data file and a logger

---
+++ mmgen/data/names.py
@@ -0,0 +1,44 @@
+"""
+Contains lists of names
+
+It might be better to keep these in csv-files, json-files etc, but this approach works while the dataset is small
+"""
+
+FIRST_NAME_MALE = set([
+    "Abraham",
+    "Andrew",
+    "Brian",
+    "Eric",
+    "Herbert",
+    "Ian",
+    "John",
+    "Stephen",
+    "Thomas",
+    "William",
+])
+
+FIRST_NAME_FEMALE = set([
+    "Anna",
+    "Barbara",
+    "Cynthia",
+    "Diane",
+    "Jane",
+    "Lisa",
+    "Mary",
+    "Molly",
+    "Patricia",
+    "Susanne",
+])
+
+LAST_NAME = set([
+    "Anderson",
+    "Brown",
+    "Davis",
+    "Johnson",
+    "Jones",
+    "Miller",
+    "Moore",
+    "Smith",
+    "Taylor",
+    "Williams",
+])
